75d1cedf222b7b2e1f2f1482cfe64bda4e7c0ff2
smrrazavian <smrrazavian@outlook.com>
2021-10-30 13:38:11 +0330

    adding constant instead of inputs

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 
-a = int(input("First Num :"))
-b = int(input("second Num :"))
+a = 1000
+b = 25
 c = a/b
+resp = """  The fist number is : {}
+            The Second Number is : {} 
+            Finaly the devision is : {}""".format(a, b, c)
 
 app  = FastAPI()
 
 @app.get("/")
 async def root():
-    return c
+    return resp
