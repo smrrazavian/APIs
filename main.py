5797b3c8927dfb447ada0a1ade46e2c73029666f
smrrazavian <smrrazavian@outlook.com>
2021-11-01 19:29:44 +0330

    Query For Inserting Numbers

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, HTTPException
+import Sqlite_query
 
 app  = FastAPI()
 
-@app.get("/{a}/{b}")
-async def root(a: float, b: float):
+@app.get("/division/{a}/{b}")
+async def adding_to_db(a: float, b: float):
     if b == 0 :
         raise HTTPException(status_code=417, detail="The second number cannot be 0 ")
 
-    c = a/b
-    return {"Num1" : a, "Num2": b, "Reseult": c}
+    result = a/b
+
+    Sqlite_query.cursor.execute("""
+        INSERT INTO NUMBER(a, b, Result)
+        VALUES({}, {}, {})
+    """.format(a, b, result))
+    
+    return {"Num1" : a, "Num2": b, "Reseult": result}
+
