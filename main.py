57881453be82e531ef0150da85b7adccff7ebd7c
smrrazavian <smrrazavian@outlook.com>
2021-11-05 00:03:38 +0330

    Adding Insert code into first endpoint

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 import models
 from Sqlite_query import SessionLocal, engine
-from sqlalchemy.orm import Session
-
+from sqlalchemy.orm import Session, session
+from models import Numbers
 
 models.Base.metadata.create_all(bind=engine)
 
 app  = FastAPI()
 
+def get_db():
+    try : 
+        db = SessionLocal()
+        yield db
+    finally :
+        db.close()
+
 @app.get("/adding/{a}/{b}")
-async def adding_to_db(a: float, b: float):
+async def adding_to_db(a: float, b: float, db: session = Depends(get_db)):
+
     if b == 0 :
         raise HTTPException(status_code=417, detail="The second number cannot be 0 ")
 
-    result = a/b
+    c = a/b
+
+    numbers = Numbers()
+    numbers.num1 = a
+    numbers.num2 = b
+    numbers.result = c
+
+    db.add(numbers)
+    db.commit()
     
-    return {"Num1" : a, "Num2": b, "Reseult": result}
+    return {"Num1" : a, "Num2": b, "Reseult": c}
