7b87575a535cab0fc57f3b8a4efe8f82911c48dd
smrrazavian <smrrazavian@outlook.com>
2021-11-08 19:36:58 +0330

    IDK

--- test_main.py
+++ test_main.py
@@ -1,8 +1,11 @@
 from fastapi.testclient import TestClient
-from .main import app
+from main import app
+from models import Numbers
+import pytest
 
 client = TestClient(app)
 
 def test_read_main():
-    response = client.get('/')
+
+    response = client.get('/add/{a}/{b}')
     assert response.status_code == 200
